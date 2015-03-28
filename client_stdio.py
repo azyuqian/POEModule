cc99db25dac5ecc6ba1aa83b767b20058c5a5c26
Yaodong Yu <yaodongy1018@gmail.com>
2015-03-28 14:53:38 -0700

    Add a basic CoAP client from aiocoap example

--- client_stdio.py
+++ client_stdio.py
@@ -1,96 +1,32 @@
-import threading
-from coapthon2.client.coap_protocol import CoAP
-from twisted.internet import reactor, stdio
-import twisted
-from twisted.protocols import basic
-import traceback
+import logging
+import asyncio
 
+from aiocoap import *
 
-# Demo the stdio input user command and get a CoAP resource
-# To run this(make sure you have a CoAP resource of /time at 127.0.0.1):
-# > python client_stdio.py
-# >> get time
+logging.basicConfig(level=logging.INFO)
 
+@asyncio.coroutine
+def main():
+    """
+    Example class which performs single PUT request to localhost
+    port 5683 (official IANA assigned CoAP port), URI "/other/block".
+    Request is sent 2 seconds after initialization.
 
-class WebCheckerCommandProtocol(basic.LineReceiver):
-    delimiter = '\n' # unix terminal style newlines. remove this line
-                     # for use with Telnet
-    prompt_string = '>>>'
+    Payload is bigger than 1kB, and thus is sent as several blocks.
+    """
 
-    def prompt(self):
-        self.transport.write(self.prompt_string)
+    context = yield from Context.create_client_context()
 
-    def connectionMade(self):
-        self.sendLine("PoE PL94 Console. Type 'help' for help.")
-        self.prompt()
+    yield from asyncio.sleep(2)
 
-    def lineReceived(self, line):
-        # Ignore blank lines
-        if not line:
-            self.prompt()
-            return
+    payload = b"The quick brown fox jumps over the lazy dog.\n" * 30
+    request = Message(code=PUT, payload=payload)
+    request.opt.uri_host = '192.168.2.20'
+    request.opt.uri_path = ("hello",)
 
-        # Parse the command
-        commandParts = line.split()
-        command = commandParts[0].lower()
-        args = commandParts[1:]
+    response = yield from context.request(request).response
 
-        # Dispatch the command to the appropriate method.  Note that all you
-        # need to do to implement a new command is add another do_* method.
-        try:
-            method = getattr(self, 'do_' + command)
-        except AttributeError, e:
-            self.sendLine('Error: no such command.')
-        else:
-            try:
-                method(*args)
-            except Exception, e:
-                self.sendLine('Error: ' + str(e))
+    print('Result: %s\n%r'%(response.code, response.payload))
 
-    def do_help(self, command=None):
-        """help [command]: List commands, or show help on the given command"""
-        if command:
-            self.sendLine(getattr(self, 'do_' + command).__doc__)
-        else:
-            commands = [cmd[3:] for cmd in dir(self) if cmd.startswith('do_')]
-            self.sendLine("Valid commands: " +", ".join(commands))
-        self.prompt()
-
-    def do_quit(self):
-        """quit: Quit this session"""
-        self.sendLine('Goodbye.')
-        self.transport.loseConnection()
-
-    def response_callback(self, response):
-        print response.payload
-        with open('data.txt',"a+") as output_file:
-            output_file.write(response.payload + "\n")
-        output_file.close
-
-    def do_get(self, url):
-        """get <url>: Get a resource"""
-        try:
-            protocol.get(self.response_callback, '/' + url)
-        except:
-            print traceback.format_exc()
-
-    def __checkSuccess(self, pageData):
-        self.sendLine("Success: got %i bytes." % len(pageData))
-
-    def __checkFailure(self, failure):
-        self.sendLine("Failure: " + failure.getErrorMessage())
-
-    def connectionLost(self, reason):
-        # stop the reactor, only because this is meant to be run in Stdio.
-        reactor.stop()
-
-
-protocol = CoAP(("192.168.2.20", 5683))
-reactor.listenUDP(0, protocol)
-
-# For multicast(not tested)
-#protocol = CoAP(("192.0.0.1", 5683))
-#reactor.listenMulticast(0, protocol)
-
-stdio.StandardIO(WebCheckerCommandProtocol())
-reactor.run()
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
