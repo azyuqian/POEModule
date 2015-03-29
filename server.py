caacfa6ef8b630127ef3cfc98e556bdc5860e2a4
azyuqian <Peter@facmed002252.fom.med.ubc.ca>
2015-03-29 12:03:40 -0700

    added skeleton resource and configuration file

--- server.py
+++ server.py
@@ -9,14 +9,13 @@
 # described in the accompanying LICENSE file.
 
 import logging
-
+import json
 
 import asyncio
 
 
 import aiocoap
 from aiocoap.resource import Site
-
 
 import resources as r
 
@@ -29,16 +28,38 @@ logging.getLogger('coap-server').setLevel(logging.DEBUG)
 def main():
     """ Create resource tree """
     root = Site()
+<<<<<<< Updated upstream
 
     root.add_resource((''), r.RootResource(root))
+=======
+    
+    # default resources to add
+>>>>>>> Stashed changes
     root.add_resource(('.well-known', 'core'), r.CoreResource(root))
     root.add_resource(('hello',), r.HelloWorld())
     root.add_resource(('time',), r.LocalTime())
     root.add_resource(('alert',), r.Alert())
-    root.add_resource(('acceleration',), r.Acceleration())
-    root.add_resource(('hygrothermo', 'temperature'), r.Temperature())
-    root.add_resource(('hygrothermo', 'humidity'), r.Humidity())
-
+    
+    with open('config.json') as data_file:
+        sensorlist = json.load(data_file)
+    
+    for sensor in sensorlist['sensors']:
+        if sensor['name'] is "accelerometer":
+            print ("acceleration  added ")
+            root.add_resource(('acceleration',), r.Acceleration())
+        elif sensor['name'] == "temperature":
+            print ("temperature resource added ")
+            root.add_resource(('hygrothermo', 'temperature'), r.Temperature())
+        elif sensor['name'] == "humidity":
+            print ("humidity resource added ")
+            root.add_resource(('hygrothermo', 'humidity'), r.Humidity())
+        else:
+            print ("{} added".format(sensor['name']))
+            print ("{}".format(sensor['period']))
+            print ("{}".format(sensor['min']))
+            print ("{}".format(sensor['max']))
+            print ("{}".format(sensor['channel']))
+            root.add_resource((sensor['name'],), r.Resource_Template(sensor['name'], sensor['period'], sensor['min'], sensor['max'], sensor['channel']))
     #root.add_resource(('other', 'block'), BlockResource())
     #root.add_resource(('other', 'separate'), SeparateLargeResource())
 
